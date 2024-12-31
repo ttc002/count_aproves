74991a38a4774ec60be190dc18c44636ee0e8e02
ttc002 <shokalPetr@gmail.com>
2024-12-31 22:54:06 +0300

    port 80

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def index2():
     return render_template('delete.html', stats=stats)
 
 if __name__ == '__main__':
-    app.run(port=5000,host='0.0.0.0')
+    app.run(port=80,host='0.0.0.0')
 
 
 
